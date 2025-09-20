4836179940e290fea5c6c3f0bc9aa9453858168d
NITISHASHINY <nitisha.23bcb7211@vitapstudent.ac.in>
2025-09-20 12:38:03 +0530

    Time: 89 ms (34.06%), Space: 89 MB (19.1%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if (nums.empty()) return 0;
+
+        unordered_set<int> s(nums.begin(), nums.end()); // O(n)
+        int longest = 0;
+
+        for (int num : s) {
+            // only start when num is the first element in the sequence
+            if (s.find(num - 1) == s.end()) {
+                int currentNum = num;
+                int streak = 1;
+
+                while (s.find(currentNum + 1) != s.end()) {
+                    currentNum++;
+                    streak++;
+                }
+                longest = max(longest, streak);
+            }
+        }
+        return longest;
+    }
+};
