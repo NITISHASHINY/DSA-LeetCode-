5729db9ec03b6e670ff4352fc3da3a9673c8c87c
Reality_AI <nitishashinyb@gmail.com>
2026-04-06 21:52:35 +0530

    add: all greedy solutions

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int count = 0;
+        int i = 0,j=0;
+        sort(g.begin(), g.end());   
+        sort(s.begin(), s.end()); 
+        while(i<g.size() && j<s.size()){
+            if(s[j] >= g[i] ){
+                count++;
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+
+
+
+
+         
+
+    return count; 
+    }
+    
+};
