61eac03a599a8aef5b4cade25a384951de39cca1
Reality_AI <nitishashinyb@gmail.com>
2026-04-06 21:46:25 +0530

    add: recursion solutions

---
+++ 50_powX,n.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double p=pow(x,n);
+        return p;    
+    return 0;    
+    }
+};
