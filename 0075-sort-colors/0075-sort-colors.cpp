f26c5629442795badd6c941a33b0144ac94b74bc
NITISHASHINY <nitishashinyb@gmail.com>
2026-07-06 14:28:48 +0530

    Time: 0 ms (100%), Space: 11.7 MB (48.87%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size()-1;
+        int middle = 0;
+        while(middle<=right){
+            if(nums[middle] == 0 ){
+                swap(nums[left] , nums[middle]);
+                left++;
+                middle++;
+            }
+
+            else if(nums[middle] == 1){
+                middle++;
+            }
+            else{
+                swap(nums[right] , nums[middle]);
+                right--;
+            }
+        }
+        
+
+        
+    }
+};
